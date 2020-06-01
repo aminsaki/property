3fd7b2cee04b76fdccf3e252ebea1bf5bcb61824
aminsaki <amisnaki69@gmail.com>
2020-06-02 00:25:46 +0430

    Start the project and build the database

---
+++ app/Favorite.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+
+    protected $table ="favorites";
+
+    public function favoriteable()
+    {
+        return $this->morphTo();
+    }
+
+}
